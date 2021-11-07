c6eea8f39be9d14222d4f525bf3fbaee8f501029
unknown <business.lgn6@gmail.com>
2021-11-07 19:23:39 +0530

    initial commit

---
+++ js/modified.js
@@ -0,0 +1,15 @@
+// header logo color change
+
+// window.onload = checkLogo;
+// document.onscroll = checkLogo;
+
+// function checkLogo(){
+//     var headerElement = document.getElementsByClassName("s-header")[0];
+//     var logoMainText = document.getElementsByClassName("logo-main-text")[0];
+//     if(headerElement.classList.contains('s-header__shrink')){
+//         logoMainText.classList.add('dark-color');
+//     }
+//     else{
+//         logoMainText.classList.remove('dark-color');
+//     }
+// }
